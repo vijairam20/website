1ae5cb7b46ecea26c087d89df9166938ffdee0b7
vijairam <vijairam20@gmail.com>
2018-08-26 02:51:08 +0300

    login init

---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="login">
+        <div id="loginimg"></div>
+        <form>
+           <div class="field">
+  <label class="label">Username</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input " type="text" placeholder="Text input" >
+    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </div>
+
+<div class="field">
+  <label class="label">Password</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input " type="password" placeholder="password" >
+    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </div>
+</div> 
+           </div>
+
+           <div class="field is-grouped">
+  <div class="control">
+    <button class="button is-link">Submit</button>
+  </div>
+</div>
+</form>
+       
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#login{
+    display: grid;
+    grid-template-columns: 60% 40%;
+    height: 100vh;
+}
+
+form{
+    padding: 100px;
+}
+
+#loginimg{
+    background: url('../assets/loginimage.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
+
