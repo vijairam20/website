bc61f411a37122c8112b3d9494379b93dfea6c90
vijairam <vijairam20@gmail.com>
2018-08-26 02:51:23 +0300

    sign init

---
+++ src/views/Signup.vue
@@ -0,0 +1,100 @@
+<template>
+    <div id="signup">
+        <div id="signhero">
+
+        </div>
+    <form>
+        <div class="field">
+  <label class="label">Name</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Text input">
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Username</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input " type="text" placeholder="Text input" >
+    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </div>
+
+<div class="field">
+  <label class="label">Password</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input " type="password" placeholder="password" >
+    <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Email</label>
+  <div class="control has-icons-left has-icons-right">
+    <input class="input" type="email" placeholder="Email input" >
+    <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-exclamation-triangle"></i>
+    </span>
+  </div>
+  
+</div>
+</div>
+<div class="field">
+  <div class="control">
+    <label class="checkbox">
+      <input type="checkbox">
+      I agree to the <a href="#">terms and conditions</a>
+    </label>
+  </div>
+</div>
+
+
+
+<div class="field is-grouped">
+  <div class="control">
+    <button class="button is-link">Submit</button>
+  </div>
+  <div class="control">
+    <button class="button is-text">Cancel</button>
+  </div>
+</div>
+</form>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+form{
+    float: left;
+    padding: 100px;
+    //width: 30%;
+}
+#signup{
+    //padding: 100px 500px 100px 500px;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 60% 40%;
+   // width: 30%
+}
+
+#signhero{
+    background: url('../assets/signup.png');
+}
+</style>
