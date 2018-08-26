7321823ff567b749262a001954e7fab9ed12ba3e
vijairam <vijairam20@gmail.com>
2018-08-26 11:01:41 +0300

    Home init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="home">
-    <router-link to="/signup">signup</router-link>
-    <router-link to="/login">login</router-link>
+    <div id="navbar">
+      <span class = "logo">Quanta</span>
+    </div>
+    <div id="content">
+    <div id="cta">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit architecto consequatur, cumque dicta eos, 
+        odio veniam doloribus ipsum quasi facilis ullam. Odit minima fuga velit. Esse reprehenderit error voluptatibus 
+        atque!</p>
+    <router-link tag = "button" class="button is-link" to="/signup">signup</router-link>
+    <router-link tag = "button" class="button is-link" to="/login">login</router-link>
+    </div>
+    <div id="isohero">
+    </div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +30,37 @@ export default {
 <style lang="scss" scoped>
 #home{
 height: 100vh;
+
+}
+#navbar{
+  height: 5vh;
+  text-align: left;
+  padding-left : 20px;
+}
+#content{
+display: grid;
+grid-template-columns: 50% 50% ;
+height: 90vh;
+}
+#cta{
+  width: 100%;
+  text-align: center;
+  padding: 150px;
+  padding-bottom: 80px;
+  font-size: 2em;
+}
+
+#isohero{
+  background-color: lighten($color: red, $amount: 10%);
+}
+button{
+  margin: 35px;
+}
+
+.logo{
+  font-size: 2.5em;
+  font-family: 'Reem Kufi', sans-serif;
+  color: black;
 }
 </style>
 
