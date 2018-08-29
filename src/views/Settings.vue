cd67ae33458dd3219f7ff24d083da6ac23ab2ad9
vijairam <vijairam20@gmail.com>
2018-08-30 01:08:33 +0300

    settings init

---
+++ src/views/Settings.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="settings">
+            <h1>Settings</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#settings{
+$fc : black;
+$bg : white;
+    h1{
+        color: $fc;
+        text-align: center;
+        //TODO: change font-family 
+        font-family: 'Reem Kufi', sans-serif;
+        font-weight: 600;
+        font-size: 2em;
+    }
+    width: 100%;
+    //background: lighten($color: black, $amount: 20%);
+    background-color: $bg;
+    height: 100vh;
+}
+</style>
+
