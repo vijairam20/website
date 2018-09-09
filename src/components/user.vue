258bcc9679b61a3b707efe9660ee9d66885d83dd
vijairam <vijairam20@gmail.com>
2018-09-09 20:40:44 +0300

    user component added

---
+++ src/components/user.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="user">
+    <avatar :username=name></avatar>
+    <div id="data">
+    <h1>{{name}}</h1>
+    </div>
+</div>
+</template>
+
+<script>
+import Avatar from "vue-avatar";
+export default {
+name:'user',
+components:{
+    Avatar
+},
+data:function(){
+    return {
+   
+    }
+},props:{
+    name:String
+}
+}
+</script>
+
+<style style="scss" scoped>
+#user{
+    display: flex;
+    padding: 0.5em;
+}
+#data{
+    
+    padding: 1em;
+}
+
+h1{
+    font-size: 1em;
+    font-weight: bold;
+}
+</style>
